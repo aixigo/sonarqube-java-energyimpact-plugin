3e7b82ade5a1af7a6f7a89b9d411090e0b720a7d
Marc Schreiber <info@schrieveslaach.de>
2023-12-29 11:41:44 +0100

    Fine Tune Benchmarking
    - Add more lists that CT+ benchmarks
    - Add sizing variants/params
    - Use snapshot version of JMH to make use of Perf and RAPL
    - Fine tune data collections. Not yet done results vary in ranking and I'm not yet sure why. Maybe it is the
      laptop…

--- profiling/src/main/java/com/aixigo/energyimpact/profiling/BenchmarkRunner.java
+++ profiling/src/main/java/com/aixigo/energyimpact/profiling/BenchmarkRunner.java
@@ -1,6 +1,7 @@
 package com.aixigo.energyimpact.profiling;
 
 import org.openjdk.jmh.profile.LinuxPerfProfiler;
+import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
@@ -9,7 +10,10 @@ public class BenchmarkRunner
    public static void main( String[] args ) throws Exception
    {
       var opt = new OptionsBuilder().include( "com\\.aixigo\\.energyimpact\\.profiling\\..*" )
-         // .addProfiler( LinuxPerfProfiler.class, "events=power/energy-cores/,power/energy-ram" )
+         .addProfiler( LinuxPerfProfiler.class, "events=power/energy-cores/,power/energy-ram/" )
+         .resultFormat( ResultFormatType.JSON )
+         .forks( 5 )
+         .measurementIterations( 25 )
          .build();
 
       new Runner( opt ).run();
--- profiling/src/main/java/com/aixigo/energyimpact/profiling/collections/Lists.java
+++ profiling/src/main/java/com/aixigo/energyimpact/profiling/collections/Lists.java
@@ -2,17 +2,18 @@ package com.aixigo.energyimpact.profiling.collections;
 
 import java.util.List;
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
 
+@BenchmarkMode( Mode.SingleShotTime )
 public class Lists
 {
-   @Fork( value = 1, warmups = 1 )
    @Benchmark
    public void traversal( Blackhole blackhole, ListState state )
    {
@@ -21,7 +22,6 @@ public class Lists
       }
    }
 
-   @Fork( value = 1, warmups = 1 )
    @Benchmark
    public void sequentialGet( Blackhole blackhole, ListState state )
    {
@@ -34,27 +34,69 @@ public class Lists
    public static class ListState
    {
 
-      // TODO: Add more list implementations
-      @Param( { "java.util.ArrayList" } )
+      /**
+       * see: https://github.com/ros3cin/CTplus-profiler/blob/032542d6efdd9d00c0b999fc31ba6688456daa5a/src/main/java/ListTest.java#L105-L111
+       */
+      @Param( {
+         "java.util.ArrayList",
+         "java.util.LinkedList",
+         "org.eclipse.collections.impl.list.mutable.FastList",
+         "org.apache.commons.collections4.list.NodeCachingLinkedList",
+         "org.apache.commons.collections4.list.CursorableLinkedList",
+         "org.apache.commons.collections4.list.TreeList"
+      } )
       public String listClass;
 
       // TODO: What are the values CT+ profiler uses?
       @Param( { "100" } )
       public int capacity;
 
-      // TODO: What are the values CT+ profiler uses?
-      @Param( { "50" } )
+      /**
+       * From the paper (<a href="https://doi.org/10.1007/s10664-021-09950-y">10.1007/s10664-021-09950-y</a>).
+       * <quote>
+       * To explore the impact of different energy profiles on the energy-efficiency
+       * of Java collection implementations, six energy profiles were created for asus,
+       * namely N1, N2, N4, N8, N16, and N32. Starting with N1 (the profile used on
+       * Section 6.1), created using a specific load size for each API i.e., 15,000 for
+       * Lists, 18,750 for Sets, and 50,000 for Maps. We then proceeded to multiplying960
+       * this load size by a factor of two and then used it to create a new profile,
+       * with N2 having two times the load size of N1, N4 having four times, until the
+       * maximum of 32 times the load size of N1 with N32. These profiles were created
+       * to simulate different workload sizes a collection may face, with the smaller
+       * ones representing lightweight applications that do not depend too much on965
+       * collections and the bigger ones representing data-structure heavy applications
+       * that make more intensive usage of collections.
+       * </quote>
+       */
+      @Param( {
+         // N1
+         "15000",
+         // N2
+         "30000",
+         // N4
+         "60000",
+         // // N8
+         // "120000",
+         // // N16
+         // "240000",
+         // // N32
+         // "512000"
+      } )
       public int n;
 
       List<Integer> list;
 
       @Setup( Level.Invocation )
+      @SuppressWarnings( "unchecked" )
       public void setUp() throws Exception
       {
-         @SuppressWarnings( "unchecked" )
-         var newList = (List<Integer>) Class.forName( listClass )
-            .getDeclaredConstructor( int.class )
-            .newInstance( capacity );
+         var clazz = Class.forName( listClass );
+         List<Integer> newList;
+         try {
+            newList = (List<Integer>) clazz.getDeclaredConstructor( int.class ).newInstance( capacity );
+         } catch( NoSuchMethodException e ) {
+            newList = (List<Integer>) clazz.getDeclaredConstructor().newInstance();
+         }
 
          for( int i = 0; i < n; ++i ) {
             newList.add( i );
